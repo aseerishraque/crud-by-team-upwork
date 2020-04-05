a972eeda0bc610889809b974bf46bdc96bbfe0b4
Your Name <you@example.com>
2020-04-05 21:07:19 +0600

    commit on 'ishmam'

---
+++ resources/views/employees/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Employee info</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  
+<div class="container">
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+</div>
+
+
+   
+
+</body>
+
+</html>
